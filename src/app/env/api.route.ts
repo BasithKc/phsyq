51633f5c5a357b16324413a9a12253e7c1c1b135
Abdul Basith kc <abdulbasithkc313@gmail.com>
2025-04-26 12:01:41 +0530

    ssl installed

--- src/app/env/api.route.ts
+++ src/app/env/api.route.ts
@@ -1,4 +1,5 @@
 export const Environment = {
     // baseUrl: 'http://localhost:3000/api',
-    baseUrl: 'http://13.61.151.239:3000/api',
+    // baseUrl: 'http://13.61.151.239:3000/api',
+    baseUrl: 'http://psyqlearning.com/api',
   };
